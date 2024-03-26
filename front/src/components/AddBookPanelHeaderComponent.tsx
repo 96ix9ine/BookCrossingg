5a86f229d385d3791465e29173d9c23cf8c9f855
pavelbekarev <bekarev.ps@gmail.com>
2024-03-26 22:28:04 +0500

    доверстал AddBookPanel

--- front/src/components/AddBookPanelHeaderComponent.tsx
+++ front/src/components/AddBookPanelHeaderComponent.tsx
@@ -3,25 +3,37 @@ import {
     Text, 
     Group,
     CellButton,
-    Cell
+    Cell,
+    PanelHeaderBack,
+    PanelHeaderButton
 } from "@vkontakte/vkui";
+import { useRouteNavigator } from "@vkontakte/vk-mini-apps-router";
+import { Icon28ArrowLeftOutline } from "@vkontakte/icons";
 
 
 const AddBookPanelHeaderComponent = (): JSX.Element => {
+    const router = useRouteNavigator();
+
+
     return (
-        <PanelHeader className="addBook__panelheader">
+        <PanelHeader
+            className="addBook__panelheader"
+            before={
+                <Icon28ArrowLeftOutline style={{paddingLeft: 5}} onClick={() => router.back()}/>
+            }
+        >
             <Group className="group">
                 <Text
                     className="panelheader__title"
                 >
                     Добавить книгу
                 </Text>
-                <Cell
+                <CellButton
                     onClick={() => {}} 
                     className="cellbutton"
                 >
                     <Text className="cellbutton__text">Очистить</Text>
-                </Cell>
+                </CellButton>
             </Group>
         </PanelHeader>
     )
